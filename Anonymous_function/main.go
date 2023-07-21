2c97cf1766e43ff2922e79c0aa88ac74278172ee
tarkeshwar <tarkeshwar.kumar@intimetec.com>
2023-07-21 08:26:26 +0530

    Anonymous function

--- Anonymous_function/main.go
+++ Anonymous_function/main.go
@@ -31,9 +31,17 @@ func main() {
 	test_function(variable3)
 
 	// returning an anonymous function from another function
-	
+	variable4 := Returning_function()
+	fmt.Printf(variable4("Simon ", "comission "))
 }
 
 func test_function(a_func func(p, q string) string){
 	fmt.Println(a_func ("Saloo ", "Bhai "))
+}
+
+func Returning_function() func (p, q string) string{
+	to_return := func (p, q string) string {
+		return p+ q+ "go back"
+	}
+	return to_return
 }
