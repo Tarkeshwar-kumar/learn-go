2ca5d6c276187a4d3550570ebb1f8d1d1760344c
Kumar <tarkeshwar.kumar@hp.com>
2024-01-07 09:45:17 +0530

    Learned struct, cgroup and chroot

--- 09structs/main.go
+++ 09structs/main.go
@@ -2,15 +2,43 @@ package main
 
 import "fmt"
 
-type User struct {
-	Name       string
-	age        string
-	Profession string
+type Profession struct {
+	profession_name string
+	skill []string
+	field string
+}
+
+type Person struct {
+	name       string
+	age        int
+	profession Profession
+}
+
+func (profession *Profession) describe_profession_skill() {
+	fmt.Printf("profession %s required following skillset:\n", profession.profession_name)
+	for _, skill := range profession.skill {
+		fmt.Println("-> ", skill)
+	}
+}
+
+func (person *Person) introduce() {
+	fmt.Printf("Hello World!! My name is %s and my age is %d\n", person.name, person.age)
+	fmt.Printf("I am %s, I work in %s field\n", person.profession.profession_name, person.profession.field)
+	person.profession.describe_profession_skill()
+}
+
+func (profession *Profession) change_skill(index int, skill string) {
+	profession.skill[index] = skill
 }
 
 func main() {
 	// no inheritance in go, no super and parent
-	Chelsi := User{"Chelsi", "22", "Doctor"}
-	fmt.Println(Chelsi)
-	fmt.Println(Chelsi.age)
-}
+	go_dev_skill := []string {"Go", "Git", "gRPC", "gin", "Docker", "K8s"}
+	prof := Profession{"Go developer", go_dev_skill, "software Engineer"}
+	person := Person{"Tarak", 22, prof}
+	fmt.Println(person)
+	person.introduce()
+	prof.change_skill(5, "Kubernetes")
+	person.introduce()
+}	
+	
